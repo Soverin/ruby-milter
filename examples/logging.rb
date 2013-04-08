07ddf1e45f8df4ffa4ba2a35c9bfdd3c088c1908
Markus Strauss <Markus@ITstrauss.eu>
2013-04-08 09:51:11 +0000

    added logging-only example

---
+++ examples/logging.rb
@@ -0,0 +1,70 @@
+require File.expand_path(File.dirname(__FILE__) + "/../prmilter")
+
+class MyMilter < PRMilter::Milter
+
+  def connect( hostname, family, port, address )
+    puts "connect from #{address}:#{port} (#{hostname})."
+    return Response.continue
+  end
+
+  def helo( text )
+    puts "helo #{text}"
+    return Response.continue
+  end
+
+  def mail_from( mailfrom, esmtp_info )
+    puts "mail_from: #{mailfrom}"
+    return Response.continue
+  end
+
+  def rcpt_to( mailto, esmtp_info )
+    puts "rcpt_to: #{mailto} (#{esmtp_info})"
+    return Response.continue
+  end
+
+  def data
+    puts "data"
+    return Response.continue
+  end
+
+  def unknown( data )
+    puts "unknown smtp command: #{data}"
+    return Response.continue
+  end
+
+  def header( k, v )
+    puts "header: #{k} => #{v}"
+    @headers[k] = [] if @headers[k].nil?
+    @headers[k] <<  v
+    return Response.continue
+  end
+
+  def end_headers
+    puts "end of headers; headers: #{@headers.inspect}"
+    return Response.continue
+  end
+
+  def end_body( data )
+    puts "end of body: #{data}; body: #{@body}"
+    return Response.continue
+  end
+
+  def abort
+    puts "abort"
+    return Response.continue
+  end
+
+  def quit
+    puts "quit"
+    return Response.continue
+  end
+
+  def macro( cmd, data )
+    puts "#{PRMilter::COMMANDS[cmd]} macros: #{data.inspect}"
+    return Response.continue
+  end
+
+end
+
+PRMilter.register(MyMilter)
+PRMilter.start
